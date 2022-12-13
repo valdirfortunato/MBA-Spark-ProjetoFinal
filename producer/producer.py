f547d2263e9c13e9f2d87cb905c1f9c75f4decae
valdirfortunato <106666277+valdirfortunato@users.noreply.github.com>
2022-12-12 22:55:03 -0300

    Add files via upload

--- producer/producer.py
+++ producer/producer.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 import json
 import datetime as dt
 
-bootstrap_servers = ['localhost:9092']
+bootstrap_servers = ['kafka:9092']
 topicName = 'test'
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 key = None
@@ -11,11 +11,11 @@ headers = []
 
 #message_data = json.dumps({'name': 'Landro', 'age': 35, 'city': 'TesteTest'}).encode('utf-8') 
 
-message_data = [{'cod_cliente': 1, 'agencia': 27, 'valor_op': 100, 'tipo_op': 'saque', 'data': str(dt.date.today()), 'saldo': 123},
+message_data = [{'cod_cliente': 1, 'agencia': 27, 'valor_op': 100, 'tipo_op': 'saque', 'data': str(dt.date.today()), 'saldo': 666},
                 {'cod_cliente': 2, 'agencia': 223, 'valor_op': 500, 'tipo_op': 'deposito', 'data': str(dt.date.today()), 'saldo': 5000},
                 {'cod_cliente': 3, 'agencia': 100, 'valor_op': 500, 'tipo_op': 'deposito', 'data': str(dt.date.today()), 'saldo': 5000},
-                {'cod_cliente': 1, 'agencia': 27, 'valor_op': 1000, 'tipo_op': 'saque', 'data': str(dt.date.today()), 'saldo': 10000},
-                {'cod_cliente': 2, 'agencia': 105, 'valor_op': 300, 'tipo_op': 'deposito', 'data': str(dt.date.today()), 'saldo': 5000}]
+                {'cod_cliente': 4, 'agencia': 27, 'valor_op': 1000, 'tipo_op': 'saque', 'data': str(dt.date.today()), 'saldo': 10000},
+                {'cod_cliente': 5, 'agencia': 105, 'valor_op': 300, 'tipo_op': 'deposito', 'data': str(dt.date.today()), 'saldo': 5000}]
 
 
 
